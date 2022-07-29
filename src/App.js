a6ebca5eaf8bbb9a940dde9223c4867149fe071e
Yang <lzlyvip@gmail.com>
2022-07-29 20:22:28 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Header from './views/Header'
+import Home from "./views/Home";
+import PostDetail from "./views/PostDetail";
+import CreatePost from "./views/CreatePost";
+import Login from "./views/Login";
+import Register from "./views/Register";
+// import 'antd/dist/antd.css'
+
+import bcImg from "./assets/bgc.jpg";
+
+import "./App.scss";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-container">
+        <Header></Header>
+      </div>
+      <div className="img">
+        <img src={bcImg} alt="" />
+      </div>
+      <div className="content">
+        <Routes>
+          {/* <Route path="*" element={<App />}/> */}
+            <Route path="/home/:path" element={<Home />} />
+            <Route path="/postdetail/:postId" element={<PostDetail />} />
+            <Route path="/createpost" element={<CreatePost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+
+        </Routes>
+      </div>
     </div>
   );
 }
